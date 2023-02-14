871e8f0bc99be3451148646087adffb68aa0902a
Paw3lUlita <pawel.ulita33@gmail.com>
2023-02-14 07:51:18 +0100

    Initial commit

---
+++ src/main/java/pl/ulita/worldwidewindsurfersweatherproject/WorldwideWindsurfersWeatherProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.ulita.worldwidewindsurfersweatherproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldwideWindsurfersWeatherProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldwideWindsurfersWeatherProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ulita/worldwidewindsurfersweatherproject/WorldwideWindsurfersWeatherProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ulita.worldwidewindsurfersweatherproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldwideWindsurfersWeatherProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
