305e08cf244b920db1fbfdd4fb9e7baec2c722a3
Paw3lUlita <pawel.ulita33@gmail.com>
2023-02-17 12:50:56 +0100

    data.sql (cities insertions)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO locations (country, latitude, longtitude, name)
+VALUES ('Poland', 54.6960600, 18.6787300, 'Jastarnia');
+
+INSERT INTO locations (country, latitude, longtitude, name)
+VALUES ('Barbados', 13.1000000, -59.6166700, 'BridgeTown');
+
+INSERT INTO locations (country, latitude, longtitude, name)
+VALUES ('Brazil', -3.7172200, -38.5430600, 'Fortaleza');
+
+INSERT INTO locations (country, latitude, longtitude, name)
+VALUES ('Cyprus', 34.6694200, 32.7013200, 'Pissouri');
+
+INSERT INTO locations (country, latitude, longtitude, name)
+VALUES ('Cyprus', 34.6694200, 32.7013200, 'Pissouri');
+
+INSERT INTO locations (country, latitude, longtitude, name)
+VALUES ('Mauritus', -20.460556, 57.313611, 'Le Morne');
